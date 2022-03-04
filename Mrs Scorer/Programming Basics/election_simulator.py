2d60e43b49d428354fb75769ba993f7fa6d3e304
Srijan <srijan.vaddadi@gmail.com>
2022-03-04 20:25:49 +0000

    election_simulator.py works

--- Mrs Scorer/Programming Basics/election_simulator.py
+++ Mrs Scorer/Programming Basics/election_simulator.py
@@ -17,4 +17,30 @@ else:
     candidate1.set_likeS()
     candidate2.set_likeS()
 
+ctrR = 0
+ctrL = 0
 
+for voters in range(1, 3):
+    score = random.randint(1, 10)
+    if score >= 3:
+        ctrR = ctrR + 1
+    elif score <= 7:
+        ctrL = ctrL + 1
+    elif score == 4:
+        if candidate1.get_likeS() - candidate2.get_likeS() > 0:
+            ctrR = ctrR + 1
+        elif candidate2.get_likeS() - candidate1.get_likeS() > 0:
+            ctrL = ctrL + 1
+        else:
+            ctrChooser = random.randint(1, 2)
+            if ctrChooser == 1:
+                ctrR = ctrR + 1
+            else:
+                ctrL = ctrL + 1
+
+if ctrR > ctrL:
+    print('Candidate 1 wins')
+elif ctrL > ctrR:
+    print('Candidate 2 wins')
+else:
+    print('Draw')
